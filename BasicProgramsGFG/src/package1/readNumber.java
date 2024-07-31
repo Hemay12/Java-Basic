3755c5e8ebec2fd4e21c6e79159117ef4ac3970f
Hemay12 <hemaysorathia1@gmail.com>
2024-07-31 12:23:56 +0530

    read number from standard input

---
+++ BasicProgramsGFG/src/package1/readNumber.java
@@ -0,0 +1,13 @@
+package package1;
+import java.util.Scanner;
+
+public class readNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter your name");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("Your name is " +name);
+	}
+}
