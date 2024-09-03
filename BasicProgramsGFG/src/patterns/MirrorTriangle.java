0a09eb498968b3226044fa64ab85d8802ca271c3
Hemay12 <hemaysorathia1@gmail.com>
2024-09-03 18:59:19 +0530

    Mirror triangle

--- BasicProgramsGFG/src/patterns/MirrorTriangle.java
+++ BasicProgramsGFG/src/patterns/MirrorTriangle.java
@@ -2,15 +2,17 @@ package patterns;
 
 import java.util.Scanner;
 
-public class Pyramid {
+public class MirrorTriangle {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number");
-		int a = sc.nextInt();
+		int y = sc.nextInt();
+		int a = y*2 -1;
+		int sum =1;
 		
-		for (int i=1;i<=a;i++) {
-			for(int j=a-i;j>0;j--) {
+		for (int i=1;i<=y;i++) {
+			for(int j=y-i;j>0;j--) {
 				System.out.print(" ");
 			}
 			for(int k=1;k<i+1;k++) {
@@ -21,7 +23,22 @@ public class Pyramid {
 			}
 			System.out.println();
 		}
-
+		
+		for (int i = 1;i<y+1;i++) {
+			
+			for (int k =1;k<i;k++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=sum;j<a+1;j++) {
+				System.out.print("*");
+			}
+			sum=sum+2;
+			
+			System.out.println();
+		}
+		
+		
 	}
 
 }
