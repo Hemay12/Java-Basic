d40addb94df17a1bc8f5d6d731d0ac3daea13c9d
Hemay12 <hemaysorathia1@gmail.com>
2024-09-09 22:38:38 +0530

    Sort String

---
+++ BasicProgramsGFG/src/String/SortString.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the String");
+		String text = sc.next();
+		String output = sortstring(text);
+		
+		System.out.println(output);
+
+	}
+	
+	public static String sortstring(String text) {
+		char tempArr [] = text.toCharArray();
+		Arrays.sort(tempArr);
+		//System.out.println(tempArr.toString());
+		return new String(tempArr);
+	}
+
+}
