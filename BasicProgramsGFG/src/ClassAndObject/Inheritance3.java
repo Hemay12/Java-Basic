7bf5ef89da581b4ed24e87917241131370da739c
Hemay12 <hemaysorathia1@gmail.com>
2024-09-12 22:17:56 +0530

    Inheritance 3 (not useful)

---
+++ BasicProgramsGFG/src/ClassAndObject/Inheritance3.java
@@ -0,0 +1,36 @@
+package ClassAndObject;
+
+public class Inheritance3 {
+
+	public static void main(String[] args) {
+		A superObjA = new A();
+		superObjA.i = 10;
+		superObjA.j = 20;
+		
+		B subObjB = new B();
+		subObjB.k = 30;
+		
+		superObjA.showij();
+		subObjB.showk();
+		subObjB.add();
+
+	}
+
+}
+
+class A{
+	protected int i;
+	int j;
+	void showij() {System.out.println("i="+i+" j="+j);}
+}
+
+class B extends A{
+	int k;
+	void showk() {
+		System.out.println("k="+k);
+	}
+	
+	void add() {
+		System.out.println("i+j+k="+i+j+k);
+	}
+}
